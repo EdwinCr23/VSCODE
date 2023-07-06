df303692074621761731dda11dfa7eaecb08de4c
EdwinCr23 <117111433+EdwinCr23@users.noreply.github.com>
2023-07-06 12:39:56 -0500

    Add files via upload

---
+++ Intranet/Beneficios.js
@@ -0,0 +1,22 @@
+//var mensaje = document.getElementById('mensaje');
+
+function sodexo(){
+    let sodexo = document.getElementById("sodexo");
+    sodexo.show();
+}
+
+function bonocerrar(){
+    let sodexo = document.getElementById("sodexo");
+    sodexo.close();
+}
+
+function imprimir(){
+    let mensaje = document.getElementById("mensaje");
+    mensaje.show();
+}
+
+function cerrar(){
+    let mensaje = document.getElementById("mensaje");
+    mensaje.close();
+}
+
