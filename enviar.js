11b7008a4f8aed96648084c42e74d12393ff5e93
EdwinCr23 <117111433+EdwinCr23@users.noreply.github.com>
2023-07-06 11:11:17 -0500

    Add files via upload

---
+++ enviar.js
@@ -0,0 +1,140 @@
+function enviar(){
+    alert('Su mensaje ha sido enviado, ¡Gracias por escribirnos! ')
+}
+
+function cumple(){
+    let dlg = document.getElementById("msg");
+    dlg.show();
+}
+function cumple1(){
+    let dlg = document.getElementById("msg1");
+    dlg.show();
+}
+function cumple2(){
+    let dlg = document.getElementById("msg2");
+    dlg.show();
+}
+function cumple3(){
+    let dlg = document.getElementById("msg3");
+    dlg.show();
+}
+function cumple4(){
+    let dlg = document.getElementById("msg4");
+    dlg.show();
+}
+function cumple5(){
+    let dlg = document.getElementById("msg5");
+    dlg.show();
+}
+function cumple6(){
+    let dlg = document.getElementById("msg6");
+    dlg.show();
+}
+function cumple7(){
+    let dlg = document.getElementById("msg7");
+    dlg.show();
+}
+function cumple8(){
+    let dlg = document.getElementById("msg8");
+    dlg.show();
+}
+function cumple9(){
+    let dlg = document.getElementById("msg9");
+    dlg.show();
+}
+function cumple10(){
+    let dlg = document.getElementById("msg10");
+    dlg.show();
+}
+function cumple11(){
+    let dlg = document.getElementById("msg11");
+    dlg.show();
+}
+function cumple12(){
+    let dlg = document.getElementById("msg12");
+    dlg.show();
+}
+function cumple13(){
+    let dlg = document.getElementById("msg13");
+    dlg.show();
+}
+function cumple14(){
+    let dlg = document.getElementById("msg14");
+    dlg.show();
+}
+function cumple15(){
+    let dlg = document.getElementById("msg15");
+    dlg.show();
+}
+function cerrar(){
+    let dlg = document.getElementById("msg");
+    dlg.close();
+ }
+ function cerrar1(){
+    let dlg = document.getElementById("msg1");
+    dlg.close();
+ }
+ function cerrar2(){
+    let dlg = document.getElementById("msg2");
+    dlg.close();
+ }
+ function cerrar3(){
+    let dlg = document.getElementById("msg3");
+    dlg.close();
+ }
+ function cerrar4(){
+    let dlg = document.getElementById("msg4");
+    dlg.close();
+ }
+ function cerrar5(){
+    let dlg = document.getElementById("msg5");
+    dlg.close();
+ }
+function cerrar6(){
+   let dlg = document.getElementById("msg6");
+   dlg.close();
+}
+function cerrar7(){
+    let dlg = document.getElementById("msg7");
+    dlg.close();
+ }
+function cerrar8(){
+    let dlg = document.getElementById("msg8");
+    dlg.close();
+}
+function cerrar9(){
+    let dlg = document.getElementById("msg9");
+    dlg.close();
+}
+function cerrar10(){
+    let dlg = document.getElementById("msg10");
+    dlg.close();
+}
+function cerrar11(){
+    let dlg = document.getElementById("msg11");
+    dlg.close();
+}
+function cerrar12(){
+    let dlg = document.getElementById("msg12");
+    dlg.close();
+}
+function cerrar13(){
+    let dlg = document.getElementById("msg13");
+    dlg.close();
+}
+function cerrar14(){
+    let dlg = document.getElementById("msg14");
+    dlg.close();
+}
+function cerrar15(){
+    let dlg = document.getElementById("msg15");
+    dlg.close();
+}
+ function vacio(){
+    let dlg = document.getElementById("vac");
+    dlg.show();
+}
+function aceptar(){
+    let dlg = document.getElementById("vac");
+    dlg.close();
+ }
