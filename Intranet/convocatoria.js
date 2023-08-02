bc5217aa78f4da8cf9683192acb1477130148966
EdwinCr23 <117111433+EdwinCr23@users.noreply.github.com>
2023-08-02 14:26:35 -0500

    Delete Intranet directory

--- Intranet/convocatoria.js
+++
@@ -1,9 +0,0 @@
-function Convocatoria1(){
-    let dlg = document.getElementById("convo1");
-    dlg.show();
-}
-
-function convcerrar(){
-    let dlg = document.getElementById('convo1')
-    dlg.close();
-}
