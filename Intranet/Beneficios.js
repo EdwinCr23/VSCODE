91d29ca091eb003af4542c23ec386f37860e9588
EdwinCr23 <117111433+EdwinCr23@users.noreply.github.com>
2023-08-02 14:30:38 -0500

    ultima versión

---
+++ Intranet/Beneficios.js
@@ -0,0 +1,128 @@
+//var mensaje = document.getElementById('mensaje');
+
+function sodexo(){
+    let sodexo = document.getElementById("sodexo");
+    sodexo.show();
+}
+
+function bonocerrar(){
+    let sodexo = document.getElementById("sodexo");
+    sodexo.close();
+}
+
+function tiquet(){
+    let mensaje = document.getElementById("tiquet");
+    mensaje.show();
+}
+
+function tiquetcerrar(){
+    let mensaje = document.getElementById("tiquet");
+    mensaje.close();
+}
+
+
+function materna(){
+    let mensaje = document.getElementById("materna");
+    mensaje.show();
+}
+
+function marternocerrar(){
+    let mensaje = document.getElementById("materna");
+    mensaje.close();
+}
+
+
+function salud(){
+    let mensaje = document.getElementById("salud");
+    mensaje.show();
+}
+
+function saludcerrar(){
+    let mensaje = document.getElementById("salud");
+    mensaje.close();
+}
+
+function cumple(){
+    let mensaje = document.getElementById("cumple");
+    mensaje.show();
+}
+
+function cumplecerrar(){
+    let mensaje = document.getElementById("cumple");
+    mensaje.close();
+}
+
+function fechacerrar(){
+    let mensaje = document.getElementById("fecha");
+    mensaje.close();
+}
+
+function fecha(){
+    let mensaje = document.getElementById("fecha");
+    mensaje.show();
+}
+
+function bodacerrar(){
+    let mensaje = document.getElementById("boda");
+    mensaje.close();
+}
+
+function boda(){
+    let mensaje = document.getElementById("boda");
+    mensaje.show();
+}
+
+function bodacerrar(){
+    let mensaje = document.getElementById("boda");
+    mensaje.close();
+}
+
+function escolar(){
+    let mensaje = document.getElementById("escolar");
+    mensaje.show();
+}
+
+function escolarcerrar(){
+    let mensaje = document.getElementById("escolar");
+    mensaje.close();
+}
+
+function antiguedad(){
+    let mensaje = document.getElementById("antiguedad");
+    mensaje.show();
+}
+
+function antiguedadcerrar(){
+    let mensaje = document.getElementById("antiguedad");
+    mensaje.close();
+}
+
+function escuela(){
+    let mensaje = document.getElementById("escuela");
+    mensaje.show();
+}
+
+function formacion(){
+    let mensaje = document.getElementById("escuela");
+    mensaje.close();
+}
+
+function plancarrera(){
+    let mensaje = document.getElementById("carrera");
+    mensaje.show();
+}
+
+function carrera(){
+    let mensaje = document.getElementById("carrera");
+    mensaje.close();
+}
+
+function credito(){
+    let mensaje = document.getElementById("credito");
+    mensaje.show();
+}
+
+function creditocerrar(){
+    let mensaje = document.getElementById("credito");
+    mensaje.close();
+}
