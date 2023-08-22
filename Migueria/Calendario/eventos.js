a1e3166f80b3fd446c29823bd9d436a28e7f0043
EdwinCr23 <117111433+EdwinCr23@users.noreply.github.com>
2023-08-22 16:38:23 -0500

    Estilos y eventos js

---
+++ Migueria/Calendario/eventos.js
@@ -0,0 +1,9 @@
+function molinos3(){
+    let molinos3 = document.getElementById('molinos3')
+    molinos3.show();
+}
+
+function aceptar(){
+    let mensaje = document.getElementById('molinos3')
+    mensaje.close();
+}
